ac7ffcb397e8dc583c4938f3015971491380472b
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-03-17 11:54:56 +0100

    added Properties component

---
+++ src/Properties.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Properties.css";
+
+export default function Properties() {
+  return ( <div className="row">
+            <div className="col-6">
+            <span id="description">sunny</span>
+            <img src='../public/img/currently/01d.png' alt="sunny" className="currently-icon" id="icon" />
+           </div> 
+           <div className="col-3 property-table properties">
+            <br />
+             humidity wind
+           </div>
+           <div className="col-3 property-table">
+            <br />
+            <span id="humidity">30</span>%
+            <br />
+            <span id="wind">5</span> km/h
+           </div>
+          </div>
+
+  )
+}
