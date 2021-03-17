4404cafa9d8833f83a81350e24c8c30baba59d23
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-03-17 11:51:40 +0100

    created new App structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from "react";
-import Weather from "./Weather";
-import './App.css';
+import "./App.css";
+import City from "./City";
+import Box from "./Box";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello World!!!
-        </h1>
-        <Weather city="Berlin"/>
-      </header>
+    <div className="container">
+     <div className="frame shadow">
+      <City />
+      <Box />
+      {/* Placeholder for he new forecast */}
+     <div className="row weather-forecast" id="forecast" />
+     </div>
+     <div>
+      <h2 className="signature">
+       <a
+        className="link"
+        href="https://github.com/Avery-techdev/react-weather-app"
+        target="_blank">
+         Open-source Code
+       </a>{" "}
+        by Annabell Hauschild
+      </h2>
+     </div>
     </div>
   );
 }
-
-export default App;
