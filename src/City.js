7d877cd98f3bac7f3fcc7d055ee1a75004681100
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-03-17 11:53:21 +0100

    added City component

---
+++ src/City.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DisplayCity from "./DisplayCity";
+import Search from "./Search";
+
+export default function City() {
+  return (
+    <div className="row">
+      <DisplayCity />
+      <Search />
+    </div>
+  );
+}
