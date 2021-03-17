39339366e833ba53466e3386ac33f3868752e6c4
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-03-17 11:53:34 +0100

    added Date component

---
+++ src/Date.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Date.css";
+
+export default function Date () {
+return ( <div className="col-6" id="current-date">
+          Friday
+          <br />
+          17:06
+         </div>
+)
+}
