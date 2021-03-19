1f11dcde9b4fc8d21d07e6072d4e5993faba6085
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-03-19 11:14:11 +0100

    Updated styling and added image source

--- src/Properties.js
+++ src/Properties.js
@@ -6,7 +6,7 @@ export default function Properties() {
   return ( <div className="row">
             <div className="col-6">
             <span id="description">sunny</span>
-            <img src='../public/img/currently/01d.png' alt="sunny" className="currently-icon" id="icon" />
+            <img src='/img/currently/01d.png' alt="sunny" className="currently-icon" id="icon" />
            </div> 
            <div className="col-3 property-table properties">
             <br />
